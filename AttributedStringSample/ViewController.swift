1bab51581eaf3c22d8005ed0904c6d96c7a306e3
Zabeehullah Qayumi <32587905+zabeehqayumi@users.noreply.github.com>
2019-01-21 19:59:15 -0600

    Initial Commit

---
+++ AttributedStringSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AttributedStringSample
+//
+//  Created by Zabeehullah Qayumi on 1/21/19.
+//  Copyright © 2019 Zabeehullah Qayumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
